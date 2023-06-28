6ff468ff40d421d56bc9dd054cfb8e8ec63b53e8
risz11 <120460961+risz11@users.noreply.github.com>
2023-06-28 18:55:15 +0530

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {Tilt} from "react-tilt";
 import { motion } from "framer-motion";
-
+import "react-vertical-timeline-component/style.min.css";
 import { styles } from "../styles";
 import { github } from "../assets";
 import { SectionWrapper } from "../hoc";
